f0f805807f17e77e55d424acb9672ca182824fc8
Gaviwd0 <gaviwd@gmail.com>
2025-10-29 20:40:18 -0300

    linteando test.routes

--- src/routes/test.routes.js
+++ src/routes/test.routes.js
@@ -3,6 +3,78 @@ import { createTest, getallTest } from "../controllers/test.controller.js"
 
 const router = Router()
 
+/**
+ * @openapi
+ * /api/v1/test:
+ *   post:
+ *     summary: Crea un nuevo registro de prueba.
+ *     tags: [Test]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Mi primer test"
+ *     responses:
+ *       201:
+ *         description: Creado exitosamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 1
+ *                 name:
+ *                   type: string
+ *                   example: "Mi primer test"
+ *                 createdAt:
+ *                   type: string
+ *                   format: date-time
+ *                 updatedAt:
+ *                   type: string
+ *                   format: date-time
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.post("/", createTest)
-router.get("/",getallTest)
+
+/**
+ * @openapi
+ * /api/v1/test:
+ *   get:
+ *     summary: Obtiene todos los registros de prueba.
+ *     tags: [Test]
+ *     responses:
+ *       200:
+ *         description: Operación exitosa.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: "Mi primer test"
+ *                   createdAt:
+ *                     type: string
+ *                     format: date-time
+ *                   updatedAt:
+ *                     type: string
+ *                     format: date-time
+ *       500:
+ *         description: Error interno del servidor.
+ */
+router.get("/", getallTest)
+
 export default router
