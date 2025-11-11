cc8e2cba266f1532af373e582869cfed7dd7ff8a
Gaviwd0 <gaviwd@gmail.com>
2025-11-11 16:23:19 -0300

    falta asociar sede con direccion y carreta

---
+++ src/models/headquarter.models.js
@@ -0,0 +1,24 @@
+import { sequelize } from "../config/database/dbconfig"
+import { DataTypes } from "sequelize"
+
+// sede, crear relacion entre direccion y carrera
+export const Headquarters = sequelize.define("Headquarters", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    available: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+    }
+})
