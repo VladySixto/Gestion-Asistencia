e8142baa20537e6a3166dc44f0a6b5c7ae0d5dfa
Gaviwd0 <gaviwd@gmail.com>
2025-11-11 16:44:20 -0300

    agregando logica de correlativas intemedia

--- src/models/matter_correlative.models.js
+++ src/models/matter_correlative.models.js
@@ -1,15 +1,37 @@
-import {sequalize} from "../config/database/db.config.js"
+import {sequelize} from "../config/database/db.config.js"
 import {DataTypes} from "sequelize"
 
-export const MatterCorrelative = sequalize.define("Matter_Correlative", {
+// Por cada materia necesaria para cursar la materia x "Analisis 2", habra un registro en esta tabla intermedia relacionando la materia "Analisis 2" con otro id materia "Analisis 1"
+export const MatterCorrelative = sequelize.define("Matter_Correlative", {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
+    //materia que se va a cursar
+    matter_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "Matter",
+            key: "id"
+        }
+    },
+    //materia aprobada necesaria para cursar siguente materia.
+    prerequisite_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "Matter",
+            key: "id"
+        }
+    },
     available: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: true
-    }
+    },
+    //duplicados
+    indexes: [{ unique: true, fields: ["matter_id", "prerequisite_id"] }]
+
 })
