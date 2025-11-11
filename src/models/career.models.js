98e2f4f8239581498eb38a0a32abeb39a2bbcb87
Gaviwd0 <gaviwd@gmail.com>
2025-11-11 16:24:39 -0300

    agregando campos con respecto al diagrama (academic_plan, avaible_inscription)

--- src/models/career.models.js
+++ src/models/career.models.js
@@ -15,9 +15,19 @@ export const Career = sequelize.define("Careers",{
         type: DataTypes.STRING,
         allowNull: true
     },
-    duration_years: {
+    duration_year: {
         type: DataTypes.INTEGER(2),
         allowNull: false
+    },
+    //para poner una url al plan academico.
+    academic_academic:{
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    avaible_inscription:{
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue:true
     },
     avaible: {
         type: DataTypes.BOOLEAN,
