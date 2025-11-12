ce7aaa0203a8d606b8b5083b75d6d28dad60322d
Gaviwd0 <gaviwd@gmail.com>
2025-11-11 21:36:15 -0300

    crear asociaciones

--- src/models/init.models.js
+++ src/models/init.models.js
@@ -2,20 +2,8 @@
 import { sequelize } from "../config/database/dbconfig.js"
 import dotenv from "dotenv"
 import { Address } from "./address.models.js"
-import { Career } from "./career.models.js"
-import { CareerType } from "./career_type.models.js"
-import { Headquarters } from "./headquarter.models.js"
-import { Matter } from "./matter.models.js"
-
-import { MatterRegistration } from "./matter_registration.js"
-import { matterType } from "./matter_type.models.js"
-import { ModalityTaken } from "./modality_taken.models.js"
 import { Preceptor } from "./preceptor.models.js"
-import { Schedule } from "./schedule.models.js"
 import { Student } from "./student.models.js"
-import { StudentAttendance } from "./student_attendance.models.js"
-import { CareerCategory } from "./sub_category_career.models.js"
-import { SubCategoryMatter } from "./sub_categrory_matter.models.js"
 import { User } from "./user.models.js"
 
 dotenv.config()
@@ -31,45 +19,6 @@ export const initModels = () => {
   User.hasOne(Address, { foreignKey: "user_id" })
   Address.belongsTo(User, { foreignKey: "user_id" })
 
-  // Student relationships
-  Student.hasMany(StudentAttendance, { foreignKey: "student_id" })
-  StudentAttendance.belongsTo(Student, { foreignKey: "student_id" })
-
-  Student.belongsToMany(Matter, { through: MatterRegistration, foreignKey: "student_id" })
-  Matter.belongsToMany(Student, { through: MatterRegistration, foreignKey: "matter_id" })
-
-  // Career relationships
-  Career.belongsTo(CareerType, { foreignKey: "career_type_id" })
-  CareerType.hasMany(Career, { foreignKey: "career_type_id" })
-
-  Career.belongsTo(CareerCategory, { foreignKey: "career_category_id" })
-  CareerCategory.hasMany(Career, { foreignKey: "career_category_id" })
-
-  Career.belongsToMany(Headquarters, { through: "CareerHeadquarters" })
-  Headquarters.belongsToMany(Career, { through: "CareerHeadquarters" })
-
-  // Matter relationships
-  Matter.belongsTo(matterType, { foreignKey: "matter_type_id" })
-  matterType.hasMany(Matter, { foreignKey: "matter_type_id" })
-
-  Matter.belongsTo(SubCategoryMatter, { foreignKey: "sub_category_matter_id" })
-  SubCategoryMatter.hasMany(Matter, { foreignKey: "sub_category_matter_id" })
-
-
-  Matter.hasMany(Schedule, { foreignKey: "matter_id" })
-  Schedule.belongsTo(Matter, { foreignKey: "matter_id" })
-
-  // Headquarters relationships
-  Headquarters.hasOne(Address, { foreignKey: "headquarter_id" })
-  Address.belongsTo(Headquarters, { foreignKey: "headquarter_id" })
-
-  // Preceptor relationships
-  Preceptor.belongsToMany(Career, { through: "PreceptorCareers" })
-  Career.belongsToMany(Preceptor, { through: "PreceptorCareers" })
-
-  // ModalityTaken relationships
-  Student.belongsTo(ModalityTaken, { foreignKey: "modality_taken_id" })
-  ModalityTaken.hasMany(Student, { foreignKey: "modality_taken_id" })
 }
 
 export const initDb = async () => {
