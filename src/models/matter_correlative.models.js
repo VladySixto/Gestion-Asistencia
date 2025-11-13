2640b18ccf895c00e17ce843717b781208301ddf
Gaviwd0 <gaviwd@gmail.com>
2025-11-12 21:48:25 -0300

    asociaciones funcionando

--- src/models/matter_correlative.models.js
+++ src/models/matter_correlative.models.js
@@ -1,5 +1,6 @@
-import {sequelize} from "../config/database/dbconfig.js"
-import {DataTypes} from "sequelize"
+import { sequelize } from "../config/database/dbconfig.js"
+import { DataTypes } from "sequelize"
+import { Matter } from "./matter.models.js"
 
 // Por cada materia necesaria para cursar la materia x "Analisis 2", habra un registro en esta tabla intermedia relacionando la materia "Analisis 2" con otro id materia "Analisis 1"
 export const MatterCorrelative = sequelize.define("Matter_Correlative", {
@@ -8,10 +9,25 @@ export const MatterCorrelative = sequelize.define("Matter_Correlative", {
         primaryKey: true,
         autoIncrement: true
     },
-    available: {
+  matter_cursed: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: Matter, key: "id" },
+    onDelete: "CASCADE",
+  },
+  matter_prerrequisited: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: Matter, key: "id" },
+    onDelete: "CASCADE",
+  },
+     available: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: true
     }
 
+}, {
+  tableName: "matter_correlative",
+  timestamps: false,
 })
