a80d501b503bec538da7f5d4573eb6cf784b216b
Gaviwd0 <gaviwd@gmail.com>
2025-11-13 18:08:30 -0300

    tonumber arreglado para el hundler

--- src/handlers/error.handler.js
+++ src/handlers/error.handler.js
@@ -1,5 +1,5 @@
 export const handleError = (coderes,res, error, message = "Internal error") => {
     console.error(error)
     //devuelve el que se pasa por parametro o 500 si es nulo
-    return res.status(toNumber(coderes) || 500).json({ message, error: error.message })
+    return res.status(Number(coderes) || 500).json({ message, error: error.message })
 }
