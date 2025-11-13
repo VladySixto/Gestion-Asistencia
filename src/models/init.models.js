96fa26dfd0fce4288f9505e0a38bfa4e7e68e606
Gaviwd0 <gaviwd@gmail.com>
2025-11-12 22:14:51 -0300

    deshabilitando eslint par ala linea 20

--- src/models/init.models.js
+++ src/models/init.models.js
@@ -16,6 +16,7 @@ import { Schedule } from "./schedule.models.js"
 import { MatterRegistration } from "./matter_registration.js"
 import { ModalityTaken } from "./modality_taken.models.js"
 import { StudentAttendance } from "./student_attendance.models.js"
+// eslint-disable-next-line no-unused-vars
 import { MatterCorrelative } from "./matter_correlative.models.js"
 
 dotenv.config()
